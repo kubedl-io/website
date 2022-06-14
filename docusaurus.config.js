0ab97321671d2b702822fb61e3eb76511c170882
Jian He <jianhe788@gmail.com>
2022-06-14 15:11:24 -0700

    update docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
